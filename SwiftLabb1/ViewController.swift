e7ee973bd798faf9e80ffc7a200158c7d83740a8
ASA <asa.kwarnmark@gmail.com>
2017-02-20 11:27:09 +0100

    Main View Controller prototype

--- SwiftLabb1/ViewController.swift
+++ SwiftLabb1/ViewController.swift
@@ -15,11 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onButton(_ sender: UIButton) {
+        smallLabel.text = input.text
     }
+    
+    @IBOutlet weak var smallLabel: UILabel!
+    
 
-
+    @IBOutlet weak var input: UITextView!
+    
+    
 }
 
